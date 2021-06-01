2899be33f6f04498606fb341ad13c4bf47f250ba
Parogo_72 <76404961+Parogo72@users.noreply.github.com>
2021-06-01 20:21:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-// We listen to the resize event
 window.addEventListener('resize', () => {
-  // We execute the same script as before
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
-
-if(window.innerHeight < window.innerWidth){
-      alert("Para disfrutar al máximo de la página orienta el móvil en posición vertical"); 
+  if(window.innerHeight < window.innerWidth && window.innerWidth < 650){
+      alert("Utiliza la pantalla en vertical para disfrutar al máximo de la página"); 
  }
+});
