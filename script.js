56cf8514b71af1b73133fb749d02d09b15242ed9
Parogo_72 <76404961+Parogo72@users.noreply.github.com>
2021-06-01 20:22:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-window.addEventListener('resize', () => {
-  if(window.innerHeight < window.innerWidth && window.innerWidth < 650){
-      alert("Utiliza la pantalla en vertical para disfrutar al máximo de la página"); 
- }
-});
+
